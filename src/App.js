678fcf4dd0441b37b384b5bc2d61290b92679c3f
Thiago Dallacqua <thiagodallacqua@outlook.com>
2017-11-09 14:55:03 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import FormLogin from './components/FormLogin';
+
+class App extends Component {
+  render() {
+    return (
+      <FormLogin />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FormLogin.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+// import axios from 'axios'
+
+export default class FormLogin extends Component{
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+  }
+
+  onFormSubmit(e){
+    e.preventDefault();
+
+    let data = {
+      grant_type: 'password',
+      username: e.target.usuario.value,
+      password: e.target.senha.value,
+      scope: 'offline_access'
+    }
+
+
+
+    fetch('http://mibankws.azurewebsites.net/connect/token', {
+      headers: new Headers({
+        'Content-type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': '*',
+        'Accept': 'application/json'
+      }),
+      method: 'post',
+      body: JSON.stringify(data),
+    }).then(res => {
+      console.log(res);
+    })
+    .catch(err => console.log(err))
+
+    // axios({
+    //   method: 'post',
+    //   url: 'http://mibankws.azurewebsites.net/connect/token',
+    //   data: data
+    // })
+
+    // axios.post('http://mibankws.azurewebsites.net/connect/token', {
+    //   data
+    // })
+    // .then(res => {
+    //   console.log(res)
+    // })
+    // .catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit}>
+          <label>
+            Usuário
+            <input type="text" name="usuario" />
+          </label>
+          <label>
+            Senha
+            <input type="password" name="senha" />
+          </label>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    )
+  }
+}
